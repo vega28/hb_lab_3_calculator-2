1af2b8f1a7f6de632e473ce3c205216b08f69187
Kelsi Flatland <kelsi.flatland@gmail.com>
2021-02-25 02:28:00 +0000

    Made code more robust - harder to break!

--- calculator.py
+++ calculator.py
@@ -22,28 +22,51 @@ def get_user_input():
 tokens = ['']
 while tokens[0] != 'q':
 
-    tokens = get_user_input()
+    while True:
+        tokens = get_user_input()
+        if tokens[0] == 'q': break
+        
+        # check that user input is valid:
+        try:
+            if len(tokens) > 1:
+                num1 = float(tokens[1])
+            # check for correct number of tokens
+            if len(tokens) == 3: 
+                if tokens[0] != 'square' and tokens[0] != 'cube':
+                    num2 = float(tokens[2])
+                    break
+                else:
+                    print('You have too many inputs. Please try again.')
+            elif len(tokens) > 3:
+                print('You have too many inputs. Please try again.')
+            elif (tokens[0] != 'square' and tokens[0] != 'cube' and len(tokens) < 3) or len(tokens) <= 1:
+                print('You need more inputs. Please try again.')
+            else: break # if first token is square or cube and num1 is valid
+        except ValueError:
+            print('That is not valid input. Please try again.')
+            print('Your entry should be of the format "operator number_1 (number_2)"')
 
+    # perform the appropriate task
     if tokens[0] == '+':
-        result = add(float(tokens[1]),float(tokens[2]))
+        result = add(num1, num2)
     elif tokens[0] == '-':
-         result = subtract(float(tokens[1]),float(tokens[2]))
+         result = subtract(num1, num2)
     elif tokens[0] == '*':
-        result = multiply(float(tokens[1]),float(tokens[2]))
+        result = multiply(num1, num2)
     elif tokens[0] == '/':
-        result = divide(float(tokens[1]),float(tokens[2]))
+        result = divide(num1, num2)
     elif tokens[0] == 'square':
-        result = square(float(tokens[1]))
+        result = square(num1)
     elif tokens[0] == 'cube':
-        result = cube(float(tokens[1]))
+        result = cube(num1)
     elif tokens[0] == 'pow' or tokens[0] == 'power':
-        result = power(float(tokens[1]),float(tokens[2]))
+        result = power(num1, num2)
     elif tokens[0] == 'mod' or tokens[0] == '%':
-        result = int(mod(float(tokens[1]),float(tokens[2]))) # result should be int!
+        result = int(mod(num1, num2)) # result should be int!
+    elif tokens[0] == 'q':
+        result = 'Okay, goodbye!'
     else:
-        result = 'that is not valid input - please try again.'
+        result = 'That is not valid operator. Please try again. \nYour options are +, -, *, /, square, cube, pow, mod'
     print(result)
-
-print('okay, goodbye!') 
 
 
